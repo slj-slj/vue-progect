f31af61e555cf57b84073fa0d9a45a28cfb4f2a6
Your Namepianzhideao <2843278143@qq.com>
2019-06-02 15:44:43 +0800

    vue项目2

---
+++ vue-study/src/pages/home/components/Recommend.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+        <div  class="recommend-title">热销推荐</div>
+        <ul> 
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <img :src="item.imgUrl" class="item-img">
+          
+                <div class="item-info">
+                    <p class="item-title">{{item.itemTitle}}</p>
+                    <p class="item-desc">{{item.itemDesc}}</p>
+                    <button class="item-button">查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name:'HomeRecommend',
+    data(){
+        return {
+            recommendList:[{
+                id:'1001',
+                imgUrl:'http://img1.qunarzz.com/sight/p0/1409/05/dce6461a918adf226afd9f3119eb6b14.jpg_350x240_d76e3122.jpg',
+                itemTitle:'五虎岛',
+                itemDesc:'5A级著名景区，位于吉林省吉林市'
+            },{
+                id:'1002',
+                imgUrl:'http://img1.qunarzz.com/sight/p0/1702/98/98a890af4b2d7d89a3.img.jpg_200x200_dfc9cbbb.jpg',
+                itemTitle:'圣鑫世外桃源温泉谷',
+                itemDesc:'5A级著名景区，位于吉林省吉林市'
+            },{
+                id:'1003',
+                imgUrl:'http://img1.qunarzz.com/sight/p0/1602/9f/9fc20948e19c112b90.img.jpg_200x200_14efa636.jpg',
+                itemTitle:'吉林神农温泉度假村',
+                itemDesc:'5A级著名景区，位于吉林省吉林市'
+            },{
+                id:'1004',
+                imgUrl:'http://img1.qunarzz.com/sight/p65/201211/09/b7ac0fb597ff697a93835fbb.jpg_200x200_5f1b16a2.jpg',
+                itemTitle:'拉法山',
+                itemDesc:'5A级著名景区，位于吉林省吉林市'
+            },{
+                id:'1005',
+                imgUrl:'http://img1.qunarzz.com/sight/p0/201406/25/222944c4d364b3d6e9064465071a898e.jpg_200x200_53fe0c3c.jpg',
+                itemTitle:'五虎岛游乐园（水世界）',
+                itemDesc:'5A级著名景区，位于吉林省吉林市'
+            }]
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+    @import '~@/assets/style/mixins.styl'
+    .recommend-title
+        line-height:.8rem
+        background-color #eeeeee
+        margin-top:.2rem
+        text-indent .2rem
+        font-size 110%
+        color #333
+    .item
+        display flex
+        height 1.9rem
+        overflow hidden
+        .item-img
+            width 1.7rem
+            height 1.7rem
+            padding:.1rem
+        .item-info
+            flex 1
+            padding .1rem
+            min-width 0
+            .item-title
+                line-height .54rem
+                font-size .32rem
+                ellipsis()
+            .item-desc
+                line-height:.44rem
+                color:#ccc
+                ellipsis()
+            .item-button
+                line-height:.44rem
+                background-color #ff9300
+                padding 0 .2rem
+                border-radius .06rem
+                margin-top:.16rem
+                color:#fff
+                
+</style>
+
---
+++ vue-study/src/pages/home/components/Weekend.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+        <div  class="recommend-title">周末去哪</div>
+        <ul> 
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <div class="item-img-wrapper">
+                    <img :src="item.imgUrl" class="item-img">
+                </div>
+                <div class="item-info">
+                    <p class="item-title">{{item.itemTitle}}</p>
+                    <p class="item-desc">{{item.itemDesc}}</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name:'HomeWeekend',
+    data(){
+        return {
+            recommendList:[{
+                id:'1001',
+                imgUrl:'http://img1.qunarzz.com/sight/p0/1507/cd/f7d2751cb82027f7f477bd8d73122dbc.water.jpg_350x240_a60ef4e4.jpg',
+                itemTitle:'沈阳方特欢乐世界',
+                itemDesc:'5A级著名景区，位于吉林省吉林市'
+            },{
+                id:'1002',
+                 imgUrl:'http://img1.qunarzz.com/sight/p0/1702/87/8780653a41166464a3.img.jpg_350x240_f7953119.jpg',
+                itemTitle:'沈阳方特欢乐世界',
+                itemDesc:'5A级著名景区，位于吉林省吉林市'
+            },{
+                id:'1003',
+                imgUrl:'http://img1.qunarzz.com/sight/p0/201406/25/3cc9a304f26d96a64067926d93f76387.jpg_350x240_46128252.jpg',
+                itemTitle:'沈阳方特欢乐世界',
+                itemDesc:'5A级著名景区，位于吉林省吉林市'
+            }]
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+    @import '~@/assets/style/mixins.styl'
+    .recommend-title
+        line-height:.8rem
+        background-color #eeeeee
+        margin-top:.2rem
+        text-indent .2rem
+        font-size 110%
+        color #333
+    .item-img-wrapper
+        overflow hidden
+        height 0
+        padding-bottom 50%
+    .item-img
+        width 100%
+    .item-info
+        padding .1rem
+        min-width 0
+        .item-title
+            line-height .54rem
+            font-size .32rem
+            ellipsis()
+        .item-desc
+            line-height:.44rem
+            color:#ccc
+            ellipsis()
+            
+</style>
+
