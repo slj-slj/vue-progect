f2dae5f250f25870d9dd15de9071e8a40a44f013
Your Namepianzhideao <2843278143@qq.com>
2019-06-02 21:03:33 +0800

    vue项目2

---
+++ vue-study/src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="search">
+        <input type="text" placeholder="输入城市名或拼音" class="search-city">
+    </div>
+</template>
+<script>
+export default {
+    name:'CitySearch'
+}
+</script>
+<style lang="stylus" scoped>
+    @import '~@/assets/style/varibles.styl'
+    .search
+        padding 0 .1rem
+        height $headerHeight
+        line-height $headerHeight
+        background-color $bgColor
+        text-align center
+        .search-city
+            color:#666
+            width 96%
+            height .62rem
+            line-height .62rem
+            padding:0 .1rem
+            border-radius .06rem
+            box-sizing border-box
+</style>
+
